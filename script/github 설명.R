c5ee4f7667c1e4e8c2b02327f8760d8eadfbee40
unknown <ttl2008@daum.net>
2021-03-02 10:44:10 +0900

    script

---
+++ script/github 설명.R
@@ -0,0 +1,9 @@
+@ git clone https://github.com/R-DA-Hann/welcome2.git
+@ git clone https://github.com/R-DA-Hann/R_practice.git
+
+
+# git diff 마지막 작업
+# git add 스크립트 수정
+# git commit -m "----" 변경사항 커밋
+# git log 기록
+# git push 웹페이지와 동기화
